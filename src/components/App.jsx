e6378801ce8face03c5dd8c406cde6dababa7456
evdokimoww <La15mCrSP1>
2022-04-05 12:13:26 +0300

    react lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { lazy, useContext, Suspense } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import {
   Navigate, Route, Routes,
@@ -7,16 +7,17 @@ import { ThemeProvider } from 'styled-components';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import Sidebar from './Sidebar.jsx';
-import MagazinePage from './Pages/MagazinePage.jsx';
-import ArchivePage from './Pages/ArchivePage.jsx';
-import SearchPage from './Pages/SearchPage.jsx';
-import CertificatesPage from './Pages/CertificatesPage.jsx';
 import { DomainContext } from '../context/DomainContext.jsx';
 import getTheme from '../themes.js';
-import AbstractPage from './Pages/AbstractPage.jsx';
-import SimplePage from './Pages/SimplePage.jsx';
-import NoMatch from './Pages/NoMatch.jsx';
 import ScrollToTopWrapper from './ScrollToTop.jsx';
+
+const SimplePage = lazy(() => import('./Pages/SimplePage.jsx'));
+const ArchivePage = lazy(() => import('./Pages/ArchivePage.jsx'));
+const MagazinePage = lazy(() => import('./Pages/MagazinePage.jsx'));
+const AbstractPage = lazy(() => import('./Pages/AbstractPage.jsx'));
+const SearchPage = lazy(() => import('./Pages/SearchPage.jsx'));
+const CertificatesPage = lazy(() => import('./Pages/CertificatesPage.jsx'));
+const NoMatch = lazy(() => import('./Pages/NoMatch.jsx'));
 
 const App = () => {
   const { domain } = useContext(DomainContext);
@@ -29,17 +30,19 @@ const App = () => {
           <Row className="flex-md-row flex-column">
             <Sidebar />
             <Col md={12} lg={9} className="py-md-4 px-md-5 p-3">
-              <Routes>
-                <Route path="/" element={<Navigate to="pages/index" />} />
-                <Route path="/pages/:pageLink" element={<SimplePage />} />
-                <Route path="/magazine/archive/:pageNumber" element={<ArchivePage />} />
-                <Route path="/magazine/:numberId" element={<MagazinePage />} />
-                <Route path="/magazine/archive/number/:numberId" element={<MagazinePage />} />
-                <Route path="/magazine/:numberId/abstract" element={<AbstractPage />} />
-                <Route path="/search-results" element={<SearchPage />} />
-                <Route path="/certificates" element={<CertificatesPage />} />
-                <Route path="*" element={<NoMatch />} />
-              </Routes>
+              <Suspense fallback={<div>Загрузка...</div>}>
+                <Routes>
+                  <Route path="/" element={<Navigate to="pages/index" />} />
+                  <Route path="/pages/:pageLink" element={<SimplePage />} />
+                  <Route path="/magazine/archive/:pageNumber" element={<ArchivePage />} />
+                  <Route path="/magazine/:numberId" element={<MagazinePage />} />
+                  <Route path="/magazine/archive/number/:numberId" element={<MagazinePage />} />
+                  <Route path="/magazine/:numberId/abstract" element={<AbstractPage />} />
+                  <Route path="/search-results" element={<SearchPage />} />
+                  <Route path="/certificates" element={<CertificatesPage />} />
+                  <Route path="*" element={<NoMatch />} />
+                </Routes>
+              </Suspense>
             </Col>
           </Row>
         </Container>
--- src/themes.js
+++ src/themes.js
@@ -1,7 +1,7 @@
 const themes = {
   ScientificSearch: {
     primaryColor: '54, 108, 157',
-      headerBg: `
+    headerBg: `
       background: rgba(81,123,166,1);
       background: -moz-linear-gradient(left, rgba(81,123,166,1) 0%, rgba(101,157,207,1) 51%, rgba(7,58,106,1) 100%);
       background: -webkit-gradient(left top, right top, color-stop(0%, rgba(81,123,166,1)), color-stop(51%, rgba(101,157,207,1)), color-stop(100%, rgba(7,58,106,1)));
@@ -11,15 +11,15 @@ const themes = {
       background: linear-gradient(to right, rgba(81,123,166,1) 0%, rgba(101,157,207,1) 51%, rgba(7,58,106,1) 100%);
       filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#517ba6', endColorstr='#073a6a', GradientType=1 );
     `,
-      headerTitleColor: 'white',
-      headerSubtitleColor: 'white',
-      searchButtonColor: 'white',
-      searchButtonColorHover: '54, 108, 157',
-      searchButtonBgHover: 'white',
+    headerTitleColor: 'white',
+    headerSubtitleColor: 'white',
+    searchButtonColor: 'white',
+    searchButtonColorHover: '54, 108, 157',
+    searchButtonBgHover: 'white',
   },
   TlIc: {
     primaryColor: '34, 113, 116',
-      headerBg: `
+    headerBg: `
       background: rgba(17,102,105,1);
       background: -moz-linear-gradient(-45deg, rgba(17,102,105,1) 0%, rgba(103,173,176,1) 61%, rgba(119,186,189,1) 72%, rgba(67,134,137,1) 100%);
       background: -webkit-gradient(left top, right bottom, color-stop(0%, rgba(17,102,105,1)), color-stop(61%, rgba(103,173,176,1)), color-stop(72%, rgba(119,186,189,1)), color-stop(100%, rgba(67,134,137,1)));
@@ -29,45 +29,45 @@ const themes = {
       background: linear-gradient(135deg, rgba(17,102,105,1) 0%, rgba(103,173,176,1) 61%, rgba(119,186,189,1) 72%, rgba(67,134,137,1) 100%);
       filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#116669', endColorstr='#438689', GradientType=1 );
     `,
-      headerTitleColor: 'white',
-      headerSubtitleColor: 'white',
-      searchButtonColor: 'white',
-      searchButtonColorHover: '34, 113, 116',
-      searchButtonBgHover: 'white',
+    headerTitleColor: 'white',
+    headerSubtitleColor: 'white',
+    searchButtonColor: 'white',
+    searchButtonColorHover: '34, 113, 116',
+    searchButtonBgHover: 'white',
   },
   EconomPrav: {
     primaryColor: '103, 36, 29',
-      headerBg: `
+    headerBg: `
       background: #a34746; /* Old browsers */
       background: -moz-linear-gradient(-45deg,  #a34746 0%, #a34746 0%, #aa675a 0%, #914444 16%, #914444 48%, #a8675c 100%); /* FF3.6-15 */
       background: -webkit-linear-gradient(-45deg,  #a34746 0%,#a34746 0%,#aa675a 0%,#914444 16%,#914444 48%,#a8675c 100%); /* Chrome10-25,Safari5.1-6 */
       background: linear-gradient(135deg,  #a34746 0%,#a34746 0%,#aa675a 0%,#914444 16%,#914444 48%,#a8675c 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
       filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a34746', endColorstr='#a8675c',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
     `,
-      headerTitleColor: 'white',
-      headerSubtitleColor: 'white',
-      searchButtonColor: 'white',
-      searchButtonColorHover: '103, 36, 29',
-      searchButtonBgHover: 'white',
+    headerTitleColor: 'white',
+    headerSubtitleColor: 'white',
+    searchButtonColor: 'white',
+    searchButtonColorHover: '103, 36, 29',
+    searchButtonBgHover: 'white',
   },
   ScientificNotes: {
     primaryColor: '76, 44, 101',
-      headerBg: `
+    headerBg: `
       background: #603487; /* Old browsers */
       background: -moz-linear-gradient(-45deg,  #603487 0%, #795893 51%, #816a93 100%); /* FF3.6-15 */
       background: -webkit-linear-gradient(-45deg,  #603487 0%,#795893 51%,#816a93 100%); /* Chrome10-25,Safari5.1-6 */
       background: linear-gradient(135deg,  #603487 0%,#795893 51%,#816a93 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
       filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#603487', endColorstr='#816a93',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
     `,
-      headerTitleColor: 'white',
-      headerSubtitleColor: 'white',
-      searchButtonColor: 'white',
-      searchButtonColorHover: '76, 44, 101',
-      searchButtonBgHover: 'white',
+    headerTitleColor: 'white',
+    headerSubtitleColor: 'white',
+    searchButtonColor: 'white',
+    searchButtonColorHover: '76, 44, 101',
+    searchButtonBgHover: 'white',
   },
   Ipp: {
     primaryColor: '122, 70, 20',
-      headerBg: `
+    headerBg: `
       background: rgba(170,113,57,1);
       background: -moz-linear-gradient(-45deg, rgba(170,113,57,1) 0%, rgba(237,187,138,1) 100%);
       background: -webkit-gradient(left top, right bottom, color-stop(0%, rgba(170,113,57,1)), color-stop(100%, rgba(237,187,138,1)));
@@ -77,15 +77,15 @@ const themes = {
       background: linear-gradient(135deg, rgba(170,113,57,1) 0%, rgba(237,187,138,1) 100%);
       filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#7A4614', endColorstr='#edbb8a', GradientType=1 );
     `,
-      headerTitleColor: 'white',
-      headerSubtitleColor: 'white',
-      searchButtonColor: 'white',
-      searchButtonColorHover: '122, 70, 20',
-      searchButtonBgHover: 'white',
+    headerTitleColor: 'white',
+    headerSubtitleColor: 'white',
+    searchButtonColor: 'white',
+    searchButtonColorHover: '122, 70, 20',
+    searchButtonBgHover: 'white',
   },
   Auditorium: {
     primaryColor: '24, 84, 130',
-      headerBg: `
+    headerBg: `
       background: rgba(36,119,187,1);
       background: -moz-linear-gradient(left, rgba(36,119,187,1) 0%, rgba(49,169,221,1) 50%, rgba(36,119,187,1) 100%);
       background: -webkit-gradient(left top, right top, color-stop(0%, rgba(36,119,187,1)), color-stop(50%, rgba(49,169,221,1)), color-stop(100%, rgba(36,119,187,1)));
@@ -95,12 +95,12 @@ const themes = {
       background: linear-gradient(to right, rgba(36,119,187,1) 0%, rgba(49,169,221,1) 50%, rgba(36,119,187,1) 100%);
       filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#2477bb', endColorstr='#2477bb', GradientType=1 );
     `,
-      headerTitleColor: 'white',
-      headerSubtitleColor: 'white',
-      searchButtonColor: 'white',
-      searchButtonColorHover: '24, 84, 130',
-      searchButtonBgHover: 'white',
+    headerTitleColor: 'white',
+    headerSubtitleColor: 'white',
+    searchButtonColor: 'white',
+    searchButtonColorHover: '24, 84, 130',
+    searchButtonBgHover: 'white',
   },
-}
+};
 
 export default (domain) => themes[domain];
